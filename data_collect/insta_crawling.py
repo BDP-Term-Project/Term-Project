a2317fe24dc859ecabe40b4109c57ede9c44db6a
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2023-12-02 00:09:06 +0900

    Update insta_crawling.py

--- data_collect/insta_crawling.py
+++ data_collect/insta_crawling.py
@@ -24,3 +24,19 @@ input_pw.clear()
 input_pw.send_keys(password)
 input_pw.submit()
 time.sleep(5)
+
+# 로그인 정보 저장 여부 팝업창 제거 ("나중에 하기 버튼 클릭")
+button = driver.find_element(By.CLASS_NAME, "_ac8f")
+button.click()
+time.sleep(5)
+
+# 알림 설정 팝업창 제거 ("나중에 하기 버튼 클릭")
+button2 = driver.find_element(By.CSS_SELECTOR, "body > div.x1n2onr6.xzkaem6 > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div._a9-z > button._a9--._ap36._a9_1")
+button2.click()
+time.sleep(3)
+
+# 해시태그 검색 창에 "여행" 검색
+url = "https://www.instagram.com/explore/tags/여행/"
+driver.get(url)
+time.sleep(10)
+
